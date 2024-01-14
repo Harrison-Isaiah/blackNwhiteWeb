58abcad0404e897c20483e6d6b2fc8c992896e2f
Harrison Isaiah <97739869+Harrison-Isaiah@users.noreply.github.com>
2024-01-14 17:58:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+Shery.mouseFollower();
+Shery.makeMagnet('.magnet');
+Shery.hoverWithMediaCircle('.hvr', {
+    videos:['0.mp4','2.mp4','3.mp4'],
+});
+
+gsap.to('.fleftelem',{
+    scrollTrigger:{
+        trigger:'#fimages',
+        pin:true,
+        start: 'top top',
+        end: 'bottom bottom',
+        endTrigger:'.last',
+        scrub:1
+    },
+    y:'-300%',
+    ease:Power1
+});
+
+var sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {   
+    style: 5,
+config:{onMouse:{value:1}},
+    slideStyle: (setScroll) => {
+     sections.forEach(function(section,index){
+        ScrollTrigger.create({
+            trigger:section,
+            start:"top top",
+            scrub:1,
+            onUpdate: function(prog){
+                setScroll(prog.progress+index);
+            },
+        });
+     });
+    },
+  });
+
+
+
+
+
